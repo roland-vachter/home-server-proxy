77e47c6311bf999c9f881c3d73656126418d0ea4
Roland Vachter <roland.vachter@ft.com>
2016-01-09 22:06:08 +0200

    Initial commit.

---
+++ env.js
@@ -0,0 +1,23 @@
+const config = {
+	apiKeys: {
+		own: process.env.OWN_API_KEY,
+		server: {
+			readOnly: process.env.HOME_SERVER_READ_ONLY_API_KEY,
+			admin: process.env.HOME_SERVER_ADMIN_API_KEY
+		}
+	},
+	facebook: {
+		app: {
+			id: process.env.FACEBOOK_APP_ID,
+			secret: process.env.FACEBOOK_APP_SECRET
+		}
+	},
+	users: {}
+};
+const readOnlyUsers = process.env.READ_ONLY_USERS.split(',');
+const adminUsers = process.env.ADMIN_USERS.split(',');
+
+config.users.readOnly = readOnlyUsers || [];
+config.users.admin = adminUsers || [];
+
+module.exports = config;
---
+++ routes/index.js
@@ -0,0 +1,114 @@
+"use strict";
+
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+var httpProxy = require('http-proxy');
+var env = require('../env');
+
+
+var remoteAddress = null;
+
+
+passport.serializeUser(function(user, done) {
+	done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+	done(null, user);
+});
+
+
+passport.use(new FacebookStrategy({
+        clientID: env.facebook.app.id,
+        clientSecret: env.facebook.app.secret,
+        callbackURL: "http://localhost:8090/login/facebook/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+    	if (profile && profile.emails && profile.emails.length) {
+            let typeOfAccess = null;
+
+            for (let i = 0; i < profile.emails.length; i++) {
+                let email = profile.emails[i];
+
+                if (env.users.admin.includes(email)) {
+                    typeOfAccess = 'admin';
+                    break;
+                }
+
+                if (env.users.readOnly.includes(email)) {
+                    typeOfAccess = 'readOnly';
+                }
+            }
+
+    		if (typeOfAccess !== null) {
+                profile.accessLevel = typeOfAccess;
+    			done(null, profile);
+    			return;
+    		}
+    	}
+
+        done(new Error("Forbidden."));
+    }
+));
+
+router.post('/register', function(req, res, next) {
+    if (req.get('API_KEY') === env.apiKeys.own && req.body.hasOwnProperty('address')) {
+        remoteAddress = req.body.address;
+
+        res.sendStatus(200);
+        return;
+    }
+
+    res.sendStatus(403);
+});
+
+router.get('/forbidden', function (req, res, next) {
+	res.sendStatus(403);
+});
+
+
+router.get('/login/facebook', passport.authenticate('facebook', {
+	scope : 'email'
+}));
+ 
+// handle the callback after facebook has authenticated the user
+router.get('/login/facebook/callback', passport.authenticate('facebook', {
+	successRedirect : '/',
+	failureRedirect : '/forbidden'
+}));
+
+
+
+var proxy = httpProxy.createProxyServer({});
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+	if (req.user) {
+        if (req.user.accessLevel === 'readOnly') {
+            proxyReq.setHeader('API_KEY', env.apiKeys.server.readOnly);
+        } else if (req.user.accessLevel === 'admin') {
+            proxyReq.setHeader('API_KEY', env.apiKeys.server.admin);
+        }
+	}
+});
+router.get('/*', isLoggedIn, function(req, res, next) {
+	if (req.user) {
+		proxy.web(req, res, {
+			target: remoteAddress
+		});
+	} else {
+		res.sendStatus(403);
+	}
+});
+
+
+
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.redirect('/login/facebook');
+}
+
+module.exports = router;
